9ea1171184d27cfaed70535b69b33e5ae95a156f
Shenal Fernando <shenalfernando4422@gmail.com>
2026-08-28 19:44:55 +0530

    added the option for passengers to book orders added cancel order passenger fixed an issue where a passenger could not make multiple orders added the option for admin to update,cancel and add data to the table added the option for admin to view orders added logout button for admin

--- src/main/java/group/trainbooking/Admindashboard.java
+++ src/main/java/group/trainbooking/Admindashboard.java
@@ -4,6 +4,8 @@
  */
 package group.trainbooking;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Legion
@@ -30,14 +32,18 @@ public class Admindashboard extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        viewodrbtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Edit schedule");
+        jButton1.setText("Add/Edit schedule");
         jButton1.addActionListener(this::jButton1ActionPerformed);
 
-        jButton2.setText("Close");
+        jButton2.setText("Logout");
         jButton2.addActionListener(this::jButton2ActionPerformed);
+
+        viewodrbtn.setText("View oders");
+        viewodrbtn.addActionListener(this::viewodrbtnActionPerformed);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -46,17 +52,20 @@ public class Admindashboard extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(96, 96, 96)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 241, Short.MAX_VALUE)
+                .addGap(73, 73, 73)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(99, 99, 99))
+                .addGap(88, 88, 88)
+                .addComponent(viewodrbtn, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(53, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(317, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(viewodrbtn))
                 .addGap(60, 60, 60))
         );
 
@@ -65,12 +74,24 @@ public class Admindashboard extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        JOptionPane.showMessageDialog(this,"Switching to train schedule edit");
+        admintrainedit admedit=new admintrainedit();
+        admedit.setVisible(true);
+        this.dispose();
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void viewodrbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_viewodrbtnActionPerformed
+        // TODO add your handling code here:
+        viewoders odrs=new viewoders();
+        odrs.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_viewodrbtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -100,5 +121,6 @@ public class Admindashboard extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton viewodrbtn;
     // End of variables declaration//GEN-END:variables
 }
